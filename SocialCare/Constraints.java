3748cb583f293375a041500eb7f01b1be3490f7d
LinneaGunnarsson <linnea.gunnarsson@live.com>
2018-12-04 10:24:35 +0100

    hej

--- SocialCare/Constraints.java
+++ SocialCare/Constraints.java
@@ -2,13 +2,21 @@ package SocialCare;
 
 public interface Constraints {
     private long time;
-    private T noofresponders; // What type should this be? Replace T with the desired type.
+    private int numberOfResponders; // What type should this be? Replace T with the desired type.
 
     public long getTime() {
         return time;
     }
 
-    public T getNOR() { // Replace T with the desired type.
-        return noofresponders;
+    public int getNumberOfResponders() { // Replace T with the desired type.
+        return numberOfResponders;
+    }
+
+    public long setTime(String time) {
+        this.time = time;
+    }
+
+    public int setNumberOfResponders(int noOfResponders) { // Replace T with the desired type.
+        this.noOfResponders = noOfResponders;
     }
 }
