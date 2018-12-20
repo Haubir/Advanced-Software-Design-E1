ec0538a96b46161d2814fe5c96505b59bfe00a17
Haubir <jagheterhaubir@gmail.com>
2018-12-20 13:43:33 +0100

    Tracker added

---
+++ MatchingSystem/Tracker.java
@@ -0,0 +1,42 @@
+/*
+ * Project: Advanced-Software-Design-E1 
+ * Class:   Tracker
+ *
+ * Version info
+ * Created: 12/20/18
+ * Creator: Haubir Mariwani
+ *
+ * Copyright notice
+ * Property of Fasbros IT. Do not copy, alter, distribute, or sell in any way unless given explicit permission.
+ */
+
+package MatchingSystem;
+
+/**
+ * Class summary.
+ * <p>
+ * Class Description.
+ * </p>
+ *
+ * @author Haubir Mariwani <haubir.mariwani@fasbros.it>
+ * @version JDK 1.8
+ * @since JDK 1.8
+ */
+public class Tracker {
+    private Request request;
+
+    public Tracker(Request request) {
+        this.request = request;
+        track();
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public void track() {}
+}
